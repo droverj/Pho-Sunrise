8a410db6dfe3768ddb2fe8e9a6e646cdbe37634a
Jodie Drover <jodiedrover@live.com>
2023-09-20 00:22:33 -0400

    Update routes, create navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MenuView from './views/MenuView';
 import ContactView from './views/ContactView';
 import HomeView from './views/HomeView';
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <div>
+        <Navbar />
+      <Routes>
         <Route exact path="/" component={HomeView} />
         <Route path="/menu" component={MenuView} />
         <Route path="/contact" component={ContactView} />
-      </Switch>
+      </Routes>
+      </div>
     </Router>
   );
 }
