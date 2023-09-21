b7ffb7998d3cc6ecf65f33908330da9b7fdb03e8
Jodie Drover <jodiedrover@live.com>
2023-09-21 18:01:26 -0400

    Styling for cart page and installed twilio library

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,21 @@
-// Cart.js
 import React, { useState } from 'react';
 import { useCart } from '../components/CartContext';
 import '../styles/Cart.scss';
+
+const twilio = require('twilio'); // Import Twilio library
+
+// Initialize Twilio client with your credentials
+const accountSid = 'AC5460ec42270588245f987a890de19231';
+const authToken = 'Ycede625facbef2715baaaae2f93a7f03';
+
+const client = twilio(accountSid, authToken);
 
 const Cart = () => {
   const { cart, removeFromCart } = useCart();
   const [customerInfo, setCustomerInfo] = useState({
     name: '',
-    address: '',
-    deliveryTime: '',
+    pickupTimeOption: 'asap', // Change 'now' to 'asap'
+    pickupTime: '', // To store selected pickup time
   });
 
   const handleRemove = (cartItem) => {
@@ -28,10 +35,40 @@ const Cart = () => {
     // Handle form submission and order placement logic here
   };
 
+  // Function to generate time slots
+  const generateTimeSlots = () => {
+    const now = new Date();
+    const availableDays = [0, 2, 3, 6]; // Tuesday, Wednesday, Thursday, Sunday
+    const availableStartTime = 1130; // 11:30 AM in military time
+    const availableEndTime = now.getDay() === 5 || now.getDay() === 6 ? 2030 : 1930; // Friday, Saturday: 8:30 PM, other days: 7:30 PM
+    const timeSlots = [];
+
+    // Calculate the earliest available time
+    const earliestTime = Math.max(
+      now.getHours() * 100 + now.getMinutes() + 30, // At least 30 minutes from now
+      availableStartTime
+    );
+
+    for (let time = earliestTime; time <= availableEndTime; time += 30) {
+      const hours = Math.floor(time / 100);
+      const minutes = time % 100;
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      const formattedTime =
+        hours % 12 === 0
+          ? `12:${minutes === 0 ? '00' : minutes} ${ampm}`
+          : `${hours % 12}:${minutes === 0 ? '00' : minutes} ${ampm}`;
+      timeSlots.push(formattedTime);
+    }
+
+    return timeSlots;
+  };
+
+  const timeSlots = generateTimeSlots();
+
   return (
     <div className="cart">
       {cart.length === 0 ? (
-        <p>Shopping Cart is Empty</p>
+        <p className="empty-cart">Shopping Cart is Empty</p>
       ) : (
         <>
           <ul className="cart-items">
@@ -40,7 +77,9 @@ const Cart = () => {
                 <div className="item-info">
                   {cartItem.name} - ${cartItem.price.toFixed(2)} - Quantity: {cartItem.quantity}
                 </div>
-                <button className="remove-button" onClick={() => handleRemove(cartItem)}>Remove One</button>
+                <button className="remove-button" onClick={() => handleRemove(cartItem)}>
+                  Remove One
+                </button>
               </li>
             ))}
           </ul>
@@ -61,25 +100,41 @@ const Cart = () => {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="address">Address</label>
-            <textarea
-              id="address"
-              name="address"
-              value={customerInfo.address}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="deliveryTime">Delivery Time</label>
-            <input
-              type="text"
-              id="deliveryTime"
-              name="deliveryTime"
-              value={customerInfo.deliveryTime}
-              onChange={handleChange}
-              required
-            />
+            <label>Pickup Time</label>
+            <div className="radio-group">
+              <label>
+                <input
+                  type="radio"
+                  name="pickupTimeOption"
+                  value="asap"
+                  checked={customerInfo.pickupTimeOption === 'asap'}
+                  onChange={handleChange}
+                />
+                ASAP
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="pickupTimeOption"
+                  value="later"
+                  checked={customerInfo.pickupTimeOption === 'later'}
+                  onChange={handleChange}
+                />
+                Pickup Later
+              </label>
+            </div>
+            {customerInfo.pickupTimeOption === 'later' && (
+              <select name="pickupTime" value={customerInfo.pickupTime} onChange={handleChange} required>
+                <option value="" disabled>
+                  Select Pickup Time
+                </option>
+                {timeSlots.map((timeSlot, index) => (
+                  <option key={index} value={timeSlot}>
+                    {timeSlot}
+                  </option>
+                ))}
+              </select>
+            )}
           </div>
           <button type="submit">Place Order</button>
         </form>
