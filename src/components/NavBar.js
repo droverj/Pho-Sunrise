71827c8f91f8ec37fcb01735ace9836b0708cddc
Jodie Drover <jodiedrover@live.com>
2023-09-25 16:48:43 -0400

    Move Profile component into navbar, style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,30 @@ import { Link } from 'react-router-dom';
 import CartTracker from './CartTracker';
 import LoginButton from './common/Login';
 import LogoutButton from './common/Logout';
+import Profile from './common/Profile';
 import '../styles/Navbar.scss';
 
 function Navbar() {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/menu">Menu</Link>
-        </li>
-        <LoginButton />
-        <LogoutButton />
-        <CartTracker />
+        <div className="links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menu</Link>
+          </li>
+        </div>
+        <div className="right-nav">
+          <Profile className="profile" />
+          <CartTracker className="cart-tracker" />
+          <LoginButton />
+          <LogoutButton />
+        </div>
       </ul>
     </nav>
   );
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,8 @@
-import Profile from './Profile';
 import '../../styles/Header.scss';
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="profile-container">
-        <Profile className="profile" />
-      </div>
       <div className="header-container">
         <h1 className="title">Phở Sunrise</h1>
         <h2>(519) 579-2016</h2>
