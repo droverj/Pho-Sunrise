03c3edaef10552fb9be015c27c4b5342de472d75
Jodie Drover <jodiedrover@live.com>
2023-09-25 19:40:29 -0400

    Move Header into Home component, delete Header component and style sheet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import Header from './Header';
 import '../styles/Home.scss';
 
 const Home = () => {
   return (
     <div className="home">
-      <Header className="header" />
+          <div className="header">
+      <h1 className="title">Phở Sunrise</h1>
+      <h2>Viet-Thai Restaurant</h2>
+      <h3>(519) 579 - 2016</h3>
+    </div>
       <div className="home-container">
       Hello Home
       </div>
