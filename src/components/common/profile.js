cdd3741b0e071991bfd466a7fde5a17d7904f7c2
Jodie Drover <jodiedrover@live.com>
2023-09-25 17:00:03 -0400

    Style Profile component

--- src/components/common/profile.js
+++ src/components/common/profile.js
@@ -1,5 +1,6 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import '../../styles/Profile.scss';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,7 +11,7 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
+      <div className="profile">
         <p>{user.email}</p>
       </div>
     )
