5699e860eb3438ab288675b0d7cb8bc61fced677
Jodie Drover <jodiedrover@live.com>
2023-09-25 23:14:47 -0400

    Add contact details to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,19 @@ import '../styles/Contact.scss';
 const Contact = () => {
   return (
     <div className="contact">
+      <div className="header">
+        <h1 className="title">Phở Sunrise</h1>
+        <h2>Viet-Thai Restaurant</h2>
+        <div className="store-details">
+        <h3>Phone Number:</h3><p>(519) 579 - 2016</p>
+        <h3>Address:</h3>
+        <p>
+          1400 Ottawa Street South, Unit #B22 <br />
+          Sunrise Shopping Centre, Kitchener <br />
+          Ontario, N2E 4E2 <br />
+        </p>
+        </div>
+      </div>
       Hello Contact
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import '../styles/Home.scss';
 const Home = () => {
   return (
     <div className="home">
-          <div className="header">
-      <h1 className="title">Phở Sunrise</h1>
-      <h2>Viet-Thai Restaurant</h2>
-      <h3>(519) 579 - 2016</h3>
-    </div>
+      <div className="header">
+        <h1 className="title">Phở Sunrise</h1>
+        <h2>Viet-Thai Restaurant</h2>
+        <h3>(519) 579 - 2016</h3>
+      </div>
       <div className="home-container">
-      Hello Home
+        Hello Home
       </div>
     </div>
   );
