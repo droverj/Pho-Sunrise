4b576fa08b49cf7c489850cc5cdc4c34e1b01874
Jodie Drover <jodiedrover@live.com>
2023-09-26 00:04:50 -0400

    Add pho sunrise icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import '../styles/Home.scss';
+import SteamingBowl from '../images/steaming-bowl.png'
 
 const Home = () => {
   return (
@@ -7,6 +8,10 @@ const Home = () => {
         <h1 className="title">Phở Sunrise</h1>
         <h2>Viet-Thai Restaurant</h2>
         <h3>(519) 579 - 2016</h3>
+        <img src={SteamingBowl} alt="steaming bowl icon" />
+        <p>Please inform us of any allergies prior to ordering. Thanks!</p>
+        <p>Some dishes may contain traces of shellfish and/or peanuts.</p>
+        <p>Prices subject to change without notice.</p>
       </div>
       <div className="home-container">
         Hello Home
