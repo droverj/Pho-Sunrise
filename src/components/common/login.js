1f797fe9a26b190d39b36d4a99365df3f7f740c6
Jodie Drover <jodiedrover@live.com>
2023-09-26 16:26:09 -0400

    Add uniform styling for Login and Logout button

--- src/components/common/login.js
+++ src/components/common/login.js
@@ -1,10 +1,11 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import '../../styles/LoginButtons.scss';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className='login-buttons' onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
--- src/components/common/logout.js
+++ src/components/common/logout.js
@@ -1,11 +1,12 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import '../../styles/LoginButtons.scss';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button className='login-buttons' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
     </button>
   );
