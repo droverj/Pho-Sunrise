d12eb6f0e48e6d79353246281be009030822b2a8
Jodie Drover <jodiedrover@live.com>
2023-09-27 01:26:14 -0400

    Contact page styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,58 @@
 import '../styles/Contact.scss';
+import SteamingBowl from '../images/steaming-bowl.png'
 
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="header">
-        <h1 className="title">Phở Sunrise</h1>
+      <div className="header-container">
+        <div className="header">
+          <img src={SteamingBowl} alt="steaming bowl icon" />
+          <h1 className="title">Phở Sunrise</h1>
+        </div>
+
         <h2>Viet-Thai Restaurant</h2>
-        <div className="store-details">
-        <h3>Phone Number:</h3><p>(519) 579 - 2016</p>
-        <h3>Address:</h3>
-        <p>
-          1400 Ottawa Street South, Unit #B22 <br />
-          Sunrise Shopping Centre, Kitchener <br />
-          Ontario, N2E 4E2 <br />
-        </p>
+
+        <p>Facebook Instagram</p>
+        <div className='contact-container'>
+
+          <div className='restaurant-details'>
+            <p className='contact-subtitle'>Contact</p>
+            <div className='contact-details-container'>
+              <ul className='contact-details-label'>
+                <li>Phone Number:</li>
+                <li>Address:</li>
+              </ul>
+              <ul className='contact-details-info'>
+                <li>(519) 579 - 2016</li>
+                <li>
+                  1400 Ottawa Street South - Unit B22 <br />
+                  Kitchener, ON, Canada <br />
+                  N2E 4E2
+                </li>
+              </ul>
+            </div>
+            <p className='contact-subtitle'>Hours</p>
+            <div className='hours-container'>
+              <ul className='days'>
+                <li>Sunday:</li>
+                <li>Monday:</li>
+                <li>Tuesday:</li>
+                <li>Wednesday:</li>
+                <li>Thursday:</li>
+                <li>Friday:</li>
+                <li>Saturday:</li>
+              </ul>
+              <ul className='times'>
+                <li>11:00AM - 8:00PM</li>
+                <li>CLOSED</li>
+                <li>11:00AM - 8:00PM</li>
+                <li>11:00AM - 8:00PM</li>
+                <li>11:00AM - 8:00PM</li>
+                <li>11:00AM - 9:00PM</li>
+                <li>11:00AM - 9:00PM</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
