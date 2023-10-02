14d16614adc938e7e313dbe637e810c09816b3ca
Jodie Drover <jodiedrover@live.com>
2023-10-02 17:16:40 -0400

    Edit getRatingText function in StarRating.js to return JSX with two paragraphs

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -5,15 +5,34 @@ const StarRating = ({ starCount, rating, onRatingChange }) => {
   const [hoveredRating, setHoveredRating] = useState(0);
 
   // Determine the appropriate text based on the rating value
+  // const getRatingText = () => {
+  //   if (rating === 1) {
+  //     return `You gave Phở Sunrise a ${rating} star rating. Please tell us more about your experience so we can improve.`;
+  //   } else if (rating === 2) {
+  //     return `You rated Phở Sunrise ${rating} stars. Please tell us more about your experience so we can improve.`;
+  //   } else if (rating > 2) {
+  //     return `You rated Phở Sunrise ${rating} stars! We are so pleased you enjoyed your experience with us.`;
+  //   }
+  //   return ''; 
+  // };
+
   const getRatingText = () => {
-    if (rating === 1) {
-      return `You rated Phở Sunrise ${rating} star. Please tell us more about your experience so we can improve.`;
-    } else if (rating === 2) {
-      return `You rated Phở Sunrise ${rating} stars. Please tell us more about your experience so we can improve.`;
+    if (rating < 3) {
+      return (
+        <p>
+          You gave Phở Sunrise a {rating} star rating. <br />
+          Please tell us more about your experience so we can improve.
+        </p>
+      );
     } else if (rating > 2) {
-      return `You rated Phở Sunrise ${rating} stars! We are so pleased you enjoyed your experience with us.`;
+      return (
+        <p>
+          You gave Phở Sunrise {rating} star rating! <br />
+          We are pleased you enjoyed your experience with us.
+        </p>
+      );
     }
-    return ''; // Handle other cases as needed
+    return null; // Returning null when rating is 0 or undefined
   };
 
   return (
