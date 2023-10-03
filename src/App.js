6309aefe16fbb525deb4f105aba23025016f4055
Jodie Drover <jodiedrover@live.com>
2023-10-02 23:32:23 -0400

    Install axios, testing server in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -11,6 +12,11 @@ import './App.scss';
 
 function App() {
   const [cart, setCart] = useState([]);
+  const [items, setItems] = useState([]);
+
+  const api = axios.create({
+    baseURL: 'http://localhost:1000', // Replace with server's base URL
+  });
 
   const addToCart = (item) => {
     setCart([...cart, item]);
@@ -21,6 +27,16 @@ function App() {
     setCart(updatedCart);
   };
 
+  useEffect(() => {
+    api.get('/api/items') // You don't need to specify the full URL here
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [api]);
+
   return (
     <CartProvider>
       <BrowserRouter>
@@ -30,12 +46,21 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/menu" element={<Menu addToCart={addToCart} removeFromCart={removeFromCart} />} />
+            <Route
+              path="/menu"
+              element={<Menu addToCart={addToCart} removeFromCart={removeFromCart} />}
+            />
             <Route
               path="/cart"
               element={<Cart cart={cart} removeFromCart={removeFromCart} />}
             />
           </Routes>
+          <h1>Items from Server</h1>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
         </div>
       </BrowserRouter>
     </CartProvider>
