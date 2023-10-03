440ac93180f58837684b73e51dd0ec46d234dc04
Jodie Drover <jodiedrover@live.com>
2023-10-03 17:48:44 -0400

    Add endpoints for API

--- src/App.js
+++ src/App.js
@@ -13,6 +13,40 @@ import './App.scss';
 function App() {
   const [cart, setCart] = useState([]);
 
+  // API data
+  const [users, setUsers] = useState([]);
+  const [items, setItems] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [orderItems, setOrderItems] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const usersResponse = await axios.get('http://localhost:8080/api/users');
+        setUsers(usersResponse.data);
+
+        const itemsResponse = await axios.get('http://localhost:8080/api/items');
+        setItems(itemsResponse.data);
+
+        const ordersResponse = await axios.get('http://localhost:8080/api/orders');
+        setOrders(ordersResponse.data);
+
+        const orderItemsResponse = await axios.get('http://localhost:8080/api/order-items');
+        setOrderItems(orderItemsResponse.data);
+
+        const reviewsResponse = await axios.get('http://localhost:8080/api/reviews');
+        setReviews(reviewsResponse.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle errors here (e.g., display error message to the user)
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+
   const addToCart = (item) => {
     setCart([...cart, item]);
   };
