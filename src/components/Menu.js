073e3e364fcf345c6ba5f1a244d3583f6c4b3fd0
Jodie Drover <jodiedrover@live.com>
2023-10-06 17:18:28 -0400

    Display the Vietnamese for the section titles

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,36 +4,38 @@ import { useCart } from '../components/CartContext';
 import '../styles/Menu.scss';
 
 const Menu = ({ items }) => {
-  console.log(items);
   const { addToCart } = useCart();
 
   // Group items by section and name
   const groupedItems = {};
   items.forEach((item) => {
     if (!groupedItems[item.section]) {
-      groupedItems[item.section] = {};
-    }
-    if (!groupedItems[item.section][item.name]) {
-      groupedItems[item.section][item.name] = {
-        options: [],
-        name_vietnamese: item.name_vietnamese, // Add the Vietnamese name here
+      groupedItems[item.section] = {
+        section_vietnamese: item.section_vietnamese,
+        items: {},
       };
     }
-    groupedItems[item.section][item.name].options.push(item);
+    if (!groupedItems[item.section].items[item.name]) {
+      groupedItems[item.section].items[item.name] = {
+        options: [],
+        name_vietnamese: item.name_vietnamese,
+      };
+    }
+    groupedItems[item.section].items[item.name].options.push(item);
   });
 
   return (
     <div className="menu">
       <div className="menu-sections">
-        {Object.entries(groupedItems).map(([section, sectionItems], sectionIndex) => (
+        {Object.entries(groupedItems).map(([section, sectionData], sectionIndex) => (
           <div key={sectionIndex}>
-            <h2>{section}</h2>
-            {Object.entries(sectionItems).map(([itemName, itemData], index) => (
+            <h2>{section} - {sectionData.section_vietnamese}</h2>
+            {Object.entries(sectionData.items).map(([itemName, itemData], index) => (
               <MenuSection
                 key={index}
                 itemName={itemName}
-                itemOptions={itemData.options} // Pass the options array
-                vietnameseName={itemData.name_vietnamese} // Pass the Vietnamese name
+                itemOptions={itemData.options}
+                vietnameseName={itemData.name_vietnamese}
                 addToCart={(item) => addToCart(item)}
               />
             ))}
