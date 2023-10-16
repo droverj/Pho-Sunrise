304951a048a29b72f6fb89e44d7356ffb86d6e4e
Jodie Drover <jodiedrover@live.com>
2023-10-15 23:49:24 -0400

    First draft for home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,39 @@
 import { Link } from 'react-router-dom';
 import SteamingBowl from '../images/steaming-bowl.png'
+import Poster from '../images/pho-sunrise-poster.png'
+import RightArrow from '../images/right-arrow.jpg'
+import LeftArrow from '../images/left-arrow.jpg'
 import '../styles/Home.scss';
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="header-container">
-        <img src={SteamingBowl} alt="steaming bowl icon" />
-        <h1>Phở Sunrise</h1>
-        <h2>Viet-Thai Restaurant</h2>
+
+      <div className='heading'>
+        <div className='circle-container'>
+          <img src={SteamingBowl} alt="steaming bowl icon" />
+        </div>
+        <div className='restaurant-name'>
+          <h1>Phở Sunrise</h1>
+          <h2>Viet-Thai Restaurant</h2>
+        </div>
+      </div>
+
+      <div className='home-body'>
+          <div className='description-container'>
+            <div className='restaurant-description'>Indulge your senses in an exquisite fusion of Vietnamese and Thai flavors at our restaurant.
+              Discover a world of culinary artistry where vibrant spices, fragrant herbs, and fresh ingredients
+              unite to create an unforgettable dining experience. From savory Pho to zesty Pad Thai, each dish is a
+              harmonious blend of traditions, brought to life by our passionate chefs. Join us and savor the best of
+              both worlds, where every bite is a journey through the heart of Southeast Asia.
+            </div>
+          </div>
+        <div className='poster'>
+          <img src={Poster} alt="pho sunrise poster" />
+        </div>
+      </div>
+
+      <div className="restaurant-details-container">
         <div className='restaurant-details'>
           <p>(519) 579 - 2016</p>
           <p>|</p>
@@ -24,11 +49,17 @@ const Home = () => {
             SkipTheDishes</a>
           ) Delivery</span>
       </div>
-      <Link to="/menu">
-        <button className="place-order-button" >
-          ORDER TAKEOUT
-        </button>
-      </Link>
+
+      <div className='takeout-button-container'>
+        <img src={RightArrow} className='arrow' alt="right arrow" />
+        <Link to="/menu">
+          <button className="place-order-button" >
+            ORDER TAKEOUT
+          </button>
+        </Link>
+        <img src={LeftArrow} className='arrow' alt="left arrow" />
+      </div>
+
     </div>
   );
 }
