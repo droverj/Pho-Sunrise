54ec00ef58c7936229e48e77d4f133f386847df8
Jodie Drover <jodiedrover@live.com>
2023-10-22 23:37:41 -0400

    Highlight active page in sidenav

--- src/components/common/SideNav.js
+++ src/components/common/SideNav.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../../styles/SideNav.scss';
 
 function SideNav({ width, closeNav }) {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+
   return (
-      <div className="sidenav" style={{ width }}>
-          <button onClick={closeNav}>X</button>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-        </div>
+    <div className="sidenav" style={{ width }}>
+      <button onClick={closeNav}>X</button>
+      <li>
+        <Link to="/" className={currentPath === '/' ? 'active' : ''}>Home</Link>
+      </li>
+      <li>
+        <Link to="/contact" className={currentPath === '/contact' ? 'active' : ''}>Contact</Link>
+      </li>
+      <li>
+        <Link to="/menu" className={currentPath === '/menu' ? 'active' : ''}>Menu</Link>
+      </li>
+      <li>
+        <Link to="/cart" className={currentPath === '/cart' ? 'active' : ''}>Cart</Link>
+      </li>
+    </div>
   );
 }
 
