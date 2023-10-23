36c950b878d049fb0b34561f46ac09b2f5d99ce0
Jodie Drover <jodiedrover@live.com>
2023-10-23 11:58:03 -0400

    Add container for user image in reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -17,6 +17,7 @@ const Reviews = ({ reviews }) => {
       <ul>
         {reversedReviews.map((review) => (
           <li key={review.id}>
+            <div className='user-image'></div>
             <p>"{review.comment}"</p> &nbsp;
             <p className='submitted-review-stars'>{generateStars(review.rating)}</p>
           </li>
