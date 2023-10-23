0aef633e9c797f5a02210193d508c522180fa396
Jodie Drover <jodiedrover@live.com>
2023-10-23 00:11:08 -0400

    add closeNav function to links in sidenav

--- src/components/common/SideNav.js
+++ src/components/common/SideNav.js
@@ -6,21 +6,20 @@ function SideNav({ width, closeNav }) {
   const location = useLocation();
   const currentPath = location.pathname;
 
-
   return (
     <div className="sidenav" style={{ width }}>
       <button onClick={closeNav}>X</button>
       <li>
-        <Link to="/" className={currentPath === '/' ? 'active' : ''}>Home</Link>
+        <Link to="/" className={currentPath === '/' ? 'active' : ''} onClick={closeNav}>Home</Link>
       </li>
       <li>
-        <Link to="/contact" className={currentPath === '/contact' ? 'active' : ''}>Contact</Link>
+        <Link to="/contact" className={currentPath === '/contact' ? 'active' : ''} onClick={closeNav}>Contact</Link>
       </li>
       <li>
-        <Link to="/menu" className={currentPath === '/menu' ? 'active' : ''}>Menu</Link>
+        <Link to="/menu" className={currentPath === '/menu' ? 'active' : ''} onClick={closeNav}>Menu</Link>
       </li>
       <li>
-        <Link to="/cart" className={currentPath === '/cart' ? 'active' : ''}>Cart</Link>
+        <Link to="/cart" className={currentPath === '/cart' ? 'active' : ''} onClick={closeNav}>Cart</Link>
       </li>
     </div>
   );
