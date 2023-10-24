4e1b8d1a58138dbc1599e39ad29739ebe0b8ed1f
Jodie Drover <jodiedrover@live.com>
2023-10-24 01:16:55 -0400

    Add sidenav for menu sections

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import MenuSection from './MenuSection';
 import { useCart } from '../components/CartContext';
 import { Link } from 'react-router-dom';
 import '../styles/Menu.scss';
 
 const Menu = ({ items }) => {
-  const { addToCart, totalItems } = useCart();
   const menuSectionsRef = useRef([]);
+  const { addToCart, totalItems } = useCart();
+  const [width, setWidth] = useState('0%');
+
+  const openSidenav = () => {
+    setWidth('35%');
+  };
+
+  const closeSidenav = () => {
+    setWidth('0%');
+  };
 
   // Group items by section and name
   const groupedItems = {};
@@ -28,12 +37,9 @@ const Menu = ({ items }) => {
 
   return (
     <div className="menu">
-
       {totalItems > 0 && (
         <Link to="/cart">
-          <button className="review-order">
-            View Your Order
-          </button>
+          <button className="review-order">View Your Order</button>
         </Link>
       )}
 
@@ -56,7 +62,28 @@ const Menu = ({ items }) => {
             ))}
           </ul>
         </div>
-        
+
+        <button className='show' onClick={openSidenav}>Menu Sections</button>
+
+        <div className="menu-sidenav" style={{ width }}>
+          <button className='hide' onClick={closeSidenav}>X</button>
+            <ul>
+              {Object.entries(groupedItems).map(([section], sectionIndex) => (
+                <li key={sectionIndex}>
+                  <a
+                    href={`#section-${sectionIndex}`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      menuSectionsRef.current[sectionIndex].scrollIntoView({ behavior: 'smooth' });
+                    }}
+                  >
+                    {section}
+                  </a>
+                </li>
+              ))}
+            </ul>
+        </div>
+
         {Object.entries(groupedItems).map(([section, sectionData], sectionIndex) => (
           <div key={sectionIndex} ref={(ref) => (menuSectionsRef.current[sectionIndex] = ref)}>
             <h2 id={`section-${sectionIndex}`}>
