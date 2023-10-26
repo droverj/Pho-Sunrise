f82be9d04daea213ab3f68239c101ea9c508a1ca
Jodie Drover <jodiedrover@live.com>
2023-10-26 01:32:02 -0400

    Remove unused testing divs

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -23,43 +23,37 @@ const MenuItem = ({ groupedItem }) => {
         <h3 className='vietnamese-name'>{name_vietnamese}</h3>
       </div>
 
-      <div className='test-container'>
-        <div className="item-options">
-          {item_options.map((option) => (
-            <div key={option.id} className='option'>
-              <div className='details'>
-                <span>{option.item_option}</span>
-                <span>${parseFloat(option.price).toFixed(2)}</span>
-              </div>
-              <button
-                onClick={() =>
-                  addToCart({
-                    id: option.id,
-                    name: option.name,
-                    item_option: option.item_option,
-                    price: parseFloat(option.price).toFixed(2),
-                  })
-                }
-              >
-                order
-              </button>
-
-              <div className='quantity-added-container'>
-                {getItemCount(option.id) > 0 ? (
-                  <div className='quantity-added'>
-                    <p className="count">{getItemCount(option.id)}</p>
-                    <FontAwesomeIcon icon={faCartShopping} className="cart-icon" style={{ color: '#3C4755' }} size="1x" />
-                  </div>
-                ) : null}
-              </div>
-
+      <div className="item-options">
+        {item_options.map((option) => (
+          <div key={option.id} className='option'>
+            <div className='details'>
+              <span>{option.item_option}</span>
+              <span>${parseFloat(option.price).toFixed(2)}</span>
             </div>
-          ))}
-        </div>
-        {/* PICTURE HERE */}
-        <div className='test'>test</div>
-      </div>
+            <button
+              onClick={() =>
+                addToCart({
+                  id: option.id,
+                  name: option.name,
+                  item_option: option.item_option,
+                  price: parseFloat(option.price).toFixed(2),
+                })
+              }
+            >
+              order
+            </button>
 
+            <div className='quantity-added-container'>
+              {getItemCount(option.id) > 0 ? (
+                <div className='quantity-added'>
+                  <p className="count">{getItemCount(option.id)}</p>
+                  <FontAwesomeIcon icon={faCartShopping} className="cart-icon" style={{ color: '#3C4755' }} size="1x" />
+                </div>
+              ) : null}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
