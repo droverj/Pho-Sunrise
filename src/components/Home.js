e0581d5f8cc0265be3830721d284b8ef8fef6df1
Jodie Drover <jodiedrover@live.com>
2023-10-28 23:19:40 -0400

    Move restaurant details into container with hours

--- src/components/Home.js
+++ src/components/Home.js
@@ -34,19 +34,17 @@ const Home = () => {
         </div>
         <video className='pho-sunrise-video-banner' autoPlay>
           <source src={VideoBanner} type="video/mp4" />
-            Your browser does not support the video tag.
+          Your browser does not support the video tag.
         </video>
       </div>
-
+{/* 
       <div className="restaurant-details-container">
         <div className='restaurant-details'>
           <p className='phone-number'>(519) 579 - 2016</p>
           <span>|</span>
           <p className='address'>1400 Ottawa Street South, Kitchener ON</p>
         </div>
-      </div>
-
-      <img src={Hours} className='restaurant-hours' alt="restaurant hours" />
+      </div> */}
 
       <div className='takeout-button-container'>
         <img src={ShrimpTopLeft} className='shrimp' alt="shrimp tail" />
@@ -58,6 +56,17 @@ const Home = () => {
         <img src={ShrimpBottomRight} className='shrimp' alt="shrimp tail" />
       </div>
 
+      <div className='hours-and-location-container'>
+        <div className='contact-details-container'>
+        <p className='located'>Located in the Sunrise Shopping Centre</p>
+        <p className='phone-number'>(519) 579 - 2016</p>
+        <p className='address'>1400 Ottawa Street S<br /> Kitchener, ON</p>
+        </div>
+        <img src={Hours} className='restaurant-hours' alt="restaurant hours" />
+      </div>
+
+      <img src={BobaDrinks} className='boba-drinks' alt="boba drinks" />
+
       <div className='chefs-choice'>
         <div className='chef'>
           <h2>Chef's Choice</h2>
@@ -68,8 +77,6 @@ const Home = () => {
           <span className='price'>$14.99</span>
         </div>
       </div>
-
-      <img src={BobaDrinks} className='boba-drinks' alt="boba drinks" />
 
     </div>
   );
