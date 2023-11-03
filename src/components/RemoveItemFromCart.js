8152aa0fb60a957376a098c415eba694a73d4b81
Jodie Drover <jodiedrover@live.com>
2023-11-03 17:16:36 -0400

    Create semi-transparent overlay for creating visual effect to focus on remove-item-from-cart

--- src/components/RemoveItemFromCart.js
+++ src/components/RemoveItemFromCart.js
@@ -3,12 +3,15 @@ import '../styles/RemoveItemFromCart.scss';
 
 const RemoveItemFromCart = ({ item, setShowRemoveItemComponent, handleDeleteItem }) => {
   return (
-    <div className='remove-item-from-cart'>
-      <button className="close-btn" onClick={() => setShowRemoveItemComponent(false)}>X</button>
-      <h4>Remove {item.quantity} {item.name} from cart?</h4>
-      <button onClick={() => setShowRemoveItemComponent(false)}>Return to Cart</button>
-      <button onClick={() => handleDeleteItem(item)}>Delete Item from Cart</button>
-    </div>
+    <>
+    <div className='overlay'></div>
+      <div className='remove-item-from-cart'>
+        <button className="close-btn" onClick={() => setShowRemoveItemComponent(false)}>X</button>
+        <h4>Remove {item.quantity} {item.name} from cart?</h4>
+        <button onClick={() => setShowRemoveItemComponent(false)}>Return to Cart</button>
+        <button onClick={() => handleDeleteItem(item)}>Delete Item from Cart</button>
+      </div>
+    </>
   )
 };
 
