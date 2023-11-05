d462b9a571cea19e30edb86b4cf3cdd80eb3da8a
Jodie Drover <jodiedrover@live.com>
2023-11-05 18:00:22 -0500

    style confirmation container

--- src/components/ReviewInteractionMessage.js
+++ src/components/ReviewInteractionMessage.js
@@ -1,20 +1,26 @@
+import React from 'react';
+import '../styles/ReviewInteractionMessage.scss';
+
 const ReviewInteractionMessage = ({ reviews, userId, deleteConfirmed, reviewSubmitted, setDeleteConfirmed, handleDeleteReview }) => {
   return (
     <div>
       {deleteConfirmed ? (
         reviews.map((review) => (
           review.user_id === userId && (
-            <div key={review.id} className='delete-review-confirmation'>
-              <h3>Are you sure you want to delete your Phở Sunrise review?</h3>
-              <p>You rated Phở Sunrise {review.rating}/5 stars.</p>
-              <p>"{review.comment}"</p>
-              <div className='confirmation'>
-                <p>Delete your review?</p>
-                <p className='warning'>This action is final.</p>
+            <>
+              <div className="overlay"></div>
+              <div key={review.id} className='delete-review-confirmation'>
+                <h3>Are you sure you want to delete your Phở Sunrise review?</h3>
+                <p>You rated Phở Sunrise {review.rating}/5 stars.</p>
+                <p>"{review.comment}"</p>
+                <div className='confirmation'>
+                  <p>Delete your review?</p>
+                  <p className='warning'>This action is final.</p>
+                </div>
+                <button onClick={() => setDeleteConfirmed(null)}>Return</button>
+                <button className="danger-button" onClick={() => handleDeleteReview(review)}>Delete</button>
               </div>
-              <button onClick={() => setDeleteConfirmed(null)}>Return</button>
-              <button className="danger-button" onClick={() => handleDeleteReview(review)}>Delete</button>
-            </div>
+            </>
           )
         ))
       ) : (
