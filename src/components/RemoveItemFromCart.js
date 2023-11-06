bdddd0ee68a023da82ea5500e6c2780cc4e563a7
Jodie Drover <jodiedrover@live.com>
2023-11-06 02:38:35 -0500

    Add function to check if ordering is available

--- src/components/RemoveItemFromCart.js
+++ src/components/RemoveItemFromCart.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/RemoveItemFromCart.scss';
 
 const RemoveItemFromCart = ({ item, setShowRemoveItemComponent, handleDeleteItem }) => {
-  console.log(item);
   return (
     <>
       <div className='overlay'></div>
