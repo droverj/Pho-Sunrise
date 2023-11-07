f64135b6d7e7714e758ea08c59ba892ebaf9d5d5
Jodie Drover <jodiedrover@live.com>
2023-11-07 02:20:30 -0500

    edit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -70,53 +70,53 @@ const Cart = () => {
 
   return (
     <>
-    {totalItems > 0 ? (
-    <div className="cart">
-      <img src={ShrimpTopLeft} className='shrimp1' alt="shrimp tail" />
-      <img src={ShrimpBottomRight} className='shrimp2' alt="shrimp tail" />
-      <img src={ShrimpTopLeft} className='shrimp3' alt="shrimp tail" />
-      <img src={ShrimpBottomRight} className='shrimp4' alt="shrimp tail" />
-      <img src={ShrimpTopLeft} className='shrimp5' alt="shrimp tail" />
+      {totalItems > 0 ? (
+        <div className="cart">
+          <img src={ShrimpTopLeft} className='shrimp1' alt="shrimp tail" />
+          <img src={ShrimpBottomRight} className='shrimp2' alt="shrimp tail" />
+          <img src={ShrimpTopLeft} className='shrimp3' alt="shrimp tail" />
+          <img src={ShrimpBottomRight} className='shrimp4' alt="shrimp tail" />
+          <img src={ShrimpTopLeft} className='shrimp5' alt="shrimp tail" />
 
-      <CartNavbar totalItems={totalItems} subtotal={subtotal} />
+          <CartNavbar totalItems={totalItems} subtotal={subtotal} />
 
-      <div className='order-navigation-container'>
-        <Link to="/menu">
-          <button className='back-button'>Menu</button>
-        </Link>
-        <div><FontAwesomeIcon icon={faArrowRightLong} className="right-arrow-icon" style={{ color: 'silver', transform: 'scaleX(1)' }} size="1x" /></div>
-        <span>Cart</span>
-      </div>
+          <div className='order-navigation-container'>
+            <Link to="/menu">
+              <button className='back-button'>Menu</button>
+            </Link>
+            <div><FontAwesomeIcon icon={faArrowRightLong} className="right-arrow-icon" style={{ color: 'silver', transform: 'scaleX(1)' }} size="1x" /></div>
+            <span>Cart</span>
+          </div>
 
-      <Link to="/menu">
-        <button className="return-to-menu">Back to Menu</button>
-      </Link>
+          <Link to="/menu">
+            <button className="return-to-menu">Back to Menu</button>
+          </Link>
 
-      <div className="cart-page-heading">
-        <h1>Review your order</h1>
-        <p className='subtotal'>subtotal: <span>${subtotal}</span></p>
-        {/* <button className="empty-cart-button" onClick={handleEmptyCart}>
+          <div className="cart-page-heading">
+            <h1>Review your order</h1>
+            <p className='subtotal'>subtotal: <span>${subtotal}</span></p>
+            {/* <button className="empty-cart-button" onClick={handleEmptyCart}>
           Empty Cart
         </button> */}
-      </div>
+          </div>
 
-      <div className="cart-items-container">
-        <ul className="cart-items">
-          {cart.map((cartItem) => (
-            <CartItem
-              key={cartItem.id}
-              cartItem={cartItem}
-              handleAdd={handleAdd}
-              handleRemove={handleRemove}
-              handleDeleteItem={handleDeleteItem}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-    ) : (
-      <EmptyCart />
-    )}
+          <div className="cart-items-container">
+            <ul className="cart-items">
+              {cart.map((cartItem) => (
+                <CartItem
+                  key={cartItem.id}
+                  cartItem={cartItem}
+                  handleAdd={handleAdd}
+                  handleRemove={handleRemove}
+                  handleDeleteItem={handleDeleteItem}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <EmptyCart />
+      )}
 
 
     </>
--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSurprise } from '@fortawesome/free-solid-svg-icons';
-import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import '../styles/EmptyCart.scss'
 
 const EmptyCart = () => {
