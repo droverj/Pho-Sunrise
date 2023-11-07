961bdd77ee083bfe10ea7f3900e77a52ff9ea603
Jodie Drover <jodiedrover@live.com>
2023-11-07 02:31:35 -0500

    Use function to check for available ordering, conditionally render order button

--- src/components/CartNavbar.js
+++ src/components/CartNavbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { canPlaceOrder } from '../utilities/canPlaceOrder';
 import '../styles/CartNavbar.scss';
 
 const CartNavbar = ({ totalItems, subtotal }) => {
+  const orderingAvailable = canPlaceOrder();
+  console.log("ordering available: ", orderingAvailable);
+  console.log("REMEMBER TO APPLY BUTTON DISABLE");
+
   return (
     <div className='cart-navbar'>
       {totalItems > 0 && (
         <>
           <p className='cart-nav-subtotal'>subtotal: <span>${subtotal}</span></p>
           <Link to="/checkout">
-            <button className="checkout-button">
-              Order
-            </button>
+            {orderingAvailable ?
+              (
+                <button className="checkout-button">Order</button>
+              ) : (
+                <button className="checkout-button-disabled" disabled>Order</button>
+              )
+            }
           </Link>
         </>
       )}
